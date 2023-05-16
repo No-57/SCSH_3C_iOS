3d110901b7f0307b01408573cef19502ebb1ed8a
Vince <vinceku@pklotcorp.com>
2023-05-16 22:54:10 +0800

    create iOS project with SwiftUI & CoreData

---
+++ SCSH_3C_iOS/SCSH_3C_iOS/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  SCSH_3C_iOS
+//
+//  Created by 辜敬閎 on 2023/5/16.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(items: [])
+        }
+    }
+}
---
+++ SCSH_3C_iOS/SCSH_3C_iOSTests/SCSH_3C_iOSTests.swift
@@ -0,0 +1,13 @@
+//
+//  SCSH_3C_iOSTests.swift
+//  SCSH_3C_iOSTests
+//
+//  Created by 辜敬閎 on 2023/5/16.
+//
+
+import XCTest
+@testable import SCSH_3C_iOS
+
+final class SCSH_3C_iOSTests: XCTestCase {
+
+}
